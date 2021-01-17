945b322607104087c5ce2cc961447cc89b4c959c
litttley <617914118@qq.com>
2021-01-17 10:03:03 +0800

    1、1.0.4版本优化遗漏文 件

--- static/login/js/signup.js
+++ static/login/js/signup.js
@@ -22,24 +22,38 @@ $(document).ready(function () {
         }
 
 
-        var username = $("#login-username").val();
-        var password = $("#login-password").val();
+        var username = $("#signup-username").val();
+        var email = $("#signup-email").val();
+        var password = $("#signup-password").val();
+        var confirm_password = $("#confirm_password").val();
+        if (password == "" || confirm_password == "") {
+            toastr.warning("密码不能为空！！！");
+            $("#submit_register").attr("disabled", false);
+            $("#submit_register").text("注册");
+            return false;
+        }
+        if (password != confirm_password) {
+            toastr.warning("密码输入不致，请重新填写！！");
+            $("#submit_register").remove("disabled");
+            $("#submit_register").attr("disabled", false);
+            return false;
+        }
         var jsondata = JSON.stringify({
             "username": username,
+            "email": email,
             "password": password,
+            "confirm_password": confirm_password
         });
         $.ajax({
-            url: "/signin",
+            url: "/signup",
             type: 'POST',
             dataType: "json",
             contentType: "application/json;charset=utf-8",
             data: jsondata,
             success: function (data) {
-                if (data.code == 200) {
-                    toastr.success("登录成功，即将跳转页面！");
-                    var allcookies = document.cookie;
-                    console.log(allcookies);
-                    setTimeout("javascript:location.href='/index'", 5000);
+                if (data.code === 200) {
+                    toastr.success("注册成功，即将跳转至登录页面！");
+                    setTimeout("javascript:location.href='/login'", 5000);
                 } else {
                     toastr.error(data.msg);
                     setTimeout(function () {
